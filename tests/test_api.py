3dc6c9e90514298e2e89b8be0418dbac94070784
ds174 <110696419+ds174@users.noreply.github.com>
2025-05-28 17:52:06 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,16 +1,16 @@
 import requests
 
-BASE_URL = "https://reqres.in/api"
+BASE_URL = "https://jsonplaceholder.typicode.com"
 
-def test_get_users():
-    response = requests.get(f"{BASE_URL}/users?page=2")
+def test_get_posts():
+    response = requests.get(f"{BASE_URL}/posts")
     assert response.status_code == 200
 
-def test_create_user():
-    payload = {"name": "morpheus", "job": "leader"}
-    response = requests.post(f"{BASE_URL}/users", json=payload)
+def test_create_post():
+    payload = {"title": "foo", "body": "bar", "userId": 1}
+    response = requests.post(f"{BASE_URL}/posts", json=payload)
     assert response.status_code == 201
 
-def test_delete_user():
-    response = requests.delete(f"{BASE_URL}/users/2")
-    assert response.status_code == 204
+def test_delete_post():
+    response = requests.delete(f"{BASE_URL}/posts/1")
+    assert response.status_code == 200  # jsonplaceholder завжди повертає 200 на DELETE
