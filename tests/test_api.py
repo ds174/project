ae3b4e4a43fdf5262aae4e772edf78bbb106ae9f
ds174 <110696419+ds174@users.noreply.github.com>
2025-05-28 17:46:34 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,14 +3,14 @@ import requests
 BASE_URL = "https://reqres.in/api"
 
 def test_get_users():
-    response = requests.get(f"{BASE_URL}/users?page=2")
+    response = requests.get(f"{BASE_URL}/api/users?page=2")
     assert response.status_code == 200
 
 def test_create_user():
     payload = {"name": "morpheus", "job": "leader"}
-    response = requests.post(f"{BASE_URL}/users", json=payload)
+    response = requests.post(f"{BASE_URL}/api/users", json=payload)
     assert response.status_code == 201
 
 def test_delete_user():
-    response = requests.delete(f"{BASE_URL}/users/2")
+    response = requests.delete(f"{BASE_URL}/api/users/2")
     assert response.status_code == 204
